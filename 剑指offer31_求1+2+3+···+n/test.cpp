134161eec69035535bc2857afc20f8cf5b96c500
浅忆梦微凉 <3538922590@qq.com>
2019-02-13 21:29:33 +0800

    方法3：新建类的方法，膜拜

--- 剑指offer31_求1+2+3+···+n/test.cpp
+++ 剑指offer31_求1+2+3+···+n/test.cpp
@@ -32,15 +32,43 @@ public:
 		return 0;
 	}
 	static int Sum_Solution(int n) {
-
-
 		static func f[2] = { Solution1, Sum_Solution };
 		return n + f[!!n](n - 1);
 	}
 };
 
+//聪明：利用新建一个类，通过开辟数组的方式，将数组的大小返回
+class Temp
+{
+public:
+	Temp(){ N++; sum += N; }
+
+	static void Reset(){ N = 0; sum = 0; }
+	static int GetSum() { return sum; }
+private:
+	static int N;
+	static int sum;
+};
+
+int Temp::N = 0;
+int Temp::sum = 0;
+
+class Solution2 {
+public:
+	int Sum_Solution(int n)
+	{
+		Temp::Reset();
+
+		Temp *a = new Temp[n];
+		delete[]a;
+		a = NULL;
+
+		return Temp::GetSum();
+	}
+};
+
 void TestFunc(){
-	Solution s;
+	Solution2 s;
 	cout << s.Sum_Solution(3) << endl;
 }
 
