3994ec8a918302fcdcd627b2cdf8fee007f89361
浅忆梦微凉 <3538922590@qq.com>
2019-02-13 21:18:32 +0800

    编译通过

---
+++ 剑指offer31_求1+2+3+···+n/test.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+
+using namespace std;
+
+//求1 + 2 + 3 + ... + n，要求不能使用乘除法、
+//for、while、if、else、switch、case等关键字及条件判断语句（A ? B : C）。
+
+class Solution {
+public:
+	int Sum_Solution(int n) {
+		int ans = n;
+		ans && (ans += Sum_Solution(n - 1));
+		return ans;
+	}
+};
+
+//class Solution {
+//public:
+//	int Sum_Solution(int n) {
+//		bool a[n][n + 1];
+//		return sizeof(a) >> 1;
+//	}
+//};
+
+void TestFunc(){
+
+}
+
+int main(){
+	TestFunc();
+	system("pause");
+	return 0;
+}
