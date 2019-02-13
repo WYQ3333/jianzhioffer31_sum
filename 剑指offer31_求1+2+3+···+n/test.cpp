63a4b63bb5ab0446174b0e21be8f15ea686b83e0
浅忆梦微凉 <3538922590@qq.com>
2019-02-13 21:25:05 +0800

    方法2：利用函数指针，大神代码

--- 剑指offer31_求1+2+3+···+n/test.cpp
+++ 剑指offer31_求1+2+3+···+n/test.cpp
@@ -5,7 +5,7 @@ using namespace std;
 //求1 + 2 + 3 + ... + n，要求不能使用乘除法、
 //for、while、if、else、switch、case等关键字及条件判断语句（A ? B : C）。
 
-class Solution {
+class Solution1 {
 public:
 	int Sum_Solution(int n) {
 		int ans = n;
@@ -14,6 +14,7 @@ public:
 	}
 };
 
+//此方法vs2013不支持可变数组
 //class Solution {
 //public:
 //	int Sum_Solution(int n) {
@@ -21,6 +22,22 @@ public:
 //		return sizeof(a) >> 1;
 //	}
 //};
+
+typedef int(*func)(int);
+
+class Solution {
+public:
+	static int Solution1(int n)
+	{
+		return 0;
+	}
+	static int Sum_Solution(int n) {
+
+
+		static func f[2] = { Solution1, Sum_Solution };
+		return n + f[!!n](n - 1);
+	}
+};
 
 void TestFunc(){
 	Solution s;
